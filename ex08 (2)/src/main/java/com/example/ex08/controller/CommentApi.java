91f28add987a7e67e86cf4aee97ca431920b08b2
songyu1903 <k3s1234@naver.com>
2024-07-10 17:53:20 +0900

    day08

--- ex08 (2)/src/main/java/com/example/ex08/controller/CommentApi.java
+++ ex08 (2)/src/main/java/com/example/ex08/controller/CommentApi.java
@@ -1,17 +1,70 @@
 package com.example.ex08.controller;
 
+import com.example.ex08.dto.CommentListDTO;
+import com.example.ex08.dto.CommentModifyDTO;
+import com.example.ex08.dto.CommentWriteDTO;
 import com.example.ex08.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class CommentApi {
     private final CommentService commentService;
 
-    @PostMapping("/v1/comments")
-    public void postComment(){
+//    URL 경로 설정시 주의사항
+//    1. 모든 자원은 복수형을 사용한다.
+//    2. 상위 자원과 하위 자원의 순서를 유의한다.
+//    3. HTTP Method는 반드시 목적에 맞게 설정한다.
 
+//    HTTP Method 별 데이터 전송 방식
+//      Get , DELETE : 쿼리 스트링 사용
+//      POST, PUT, PATCH : 바디 영역 사용
+//      PathVariable 은 전부 사용가능
+
+//    Pathvariable은 url 경로상에 만든 변수이다.
+//    해당 위치로 들어오는 값을 JAVA에서 꺼내 사용할 수 있다.
+//    @PathVariable() 어노테이션을 이용하여 특정 변수위치에 들어온 값을 원하는 매개변수로 바인딩 할 수 있다.
+    @GetMapping("/v1/boards/{boardId}/comments")
+    public List<CommentListDTO> getComments(@PathVariable("boardId") Long boardId){
+        System.out.println("boardId = " + boardId);
+        List<CommentListDTO> list = commentService.findList(boardId);
+        return list;
+    }
+
+//    @RestController 에서는 자원(데이터)를 반환한다.
+//    DTO같은 객체를 반환하면 자동으로 JSON객체로 변환되어 응답 바디에 실려간다.
+//    List를 반환하면 자동으로 JSON Array로 변환되어 응답 바디에 실려 간다.
+//    JSON 객체 : {"키":값}
+//    JSON Array : []
+//    DTO를 JSON 객체로 변환하면, 필드명을 키로 사용하고 필드에 저장된 데이터를 값으로 사용한다.
+//    Spring MVC 프레임워크를 사용하면 내장된 Jackson 라이브러리를 활용하여 자동으로 변환해준다.
+//    통신을 통해 데이터를 전송하게 되는데 전송하기 좋은 형태로 변환하는 과정을 직렬화 라고한다.
+//    우리는 DTO를 JSON 형태로 JSON 직렬화를 하여 보내는 것이다.
+
+//    @RequestBody 요청의 본문영역의 JSON데이터를 특정 객체로 역직렬화 시켜준다.
+//    JSON 역직렬화 : JSON을 우리의 객체 형태로 변환
+    @PostMapping("/v1/boards/{boardId}/comments")
+    public void postComment(@PathVariable("boardId") Long boardId,
+                            @RequestBody CommentWriteDTO commentWriteDTO,
+                            @SessionAttribute("memberId") Long memberId){
+        commentWriteDTO.setBoardId(boardId);
+        commentWriteDTO.setMemberId(memberId);
+
+        commentService.addComment(commentWriteDTO);
+    }
+
+    @PatchMapping("/v1/comments/{commentId}")
+    public void patchComments(@PathVariable("commentId") Long commentId,
+                              @RequestBody CommentModifyDTO commentModifyDTO){
+        commentModifyDTO.setCommentId(commentId);
+        commentService.modifyComment(commentModifyDTO);
+    }
+
+    @DeleteMapping("/v1/comments/{commentId}")
+    public void deleteComment(@PathVariable("commentId") Long commentId){
+        commentService.deleteComment(commentId);
     }
 }
---
+++ ex08 (2)/src/main/resources/static/js/board/detail.js
@@ -0,0 +1,76 @@
+{   // 댓글 처리
+    let $writeBtn = document.querySelector('#write-btn');
+    let $content = document.querySelector("#content")
+
+    $writeBtn.addEventListener("click" , (e) => {
+        console.log("click");
+        let content = $content.value;
+
+        console.log(content);
+        //  this
+        let target = e.target
+        let boardId = target.dataset.id;
+
+        console.log(boardId);
+
+        // {키 , 값}
+        let bodyData = {content : content};
+
+        fetch(`/v1/boards/${boardId}/comments`, {
+            method: 'POST' ,
+            // Content-Type : 우리가 보내는 데이터의 타입
+            headers: {'Content-Type' : 'application/json'},
+            // stringify() : JS객체를 JSON 형태로 변환
+            body: JSON.stringify(bodyData)
+        }).then(resp => console.log(resp))
+    });
+
+    getCommentList();
+
+    function getCommentList(){
+        let $writeBtn = document.querySelector('#write-btn')
+        let boardId = $writeBtn.dataset.id
+
+        fetch(`/v1/boards/${boardId}/comments`)
+            .then(resp => resp.json())
+            .then(list => {
+                console.log(list)
+                console.log(list[2])
+                console.log(list[2].content)
+
+                let tags = '';
+
+                list.forEach(comment => {
+                    tags += `
+                    <div class="comment-row">
+                        <div>작성자 : <span>${comment.loginId}</span> </div>
+                        <div>${comment.content}</div>
+                    </div>
+                    `;
+                });
+
+                let $commentsBox = document.querySelector('.comments-box');
+
+                $commentsBox.innerHTML = tags;
+            });
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
